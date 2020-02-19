8784accffb8550cef7e86061f340d789e5539675
student1043 <1043@holbertonschool.com>
2020-02-19 21:30:47 +0100

    Update console.py

--- console.py
+++ console.py
@@ -1,2 +1,19 @@
 #!/usr/bin/python3
-from cmd import Cmd
+""" HBNB Console """
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """ Class HBNB """
+    prompt = "(hbnb) "
+
+    def do_quit(self, inp):
+        """Quit command"""
+        return True
+
+    def do_EOF(self, inp):
+        """End OF FILE"""
+        return True
+
+    def emptyline(self):
+        """ Nothing when an empty line occurs """
+        pass
