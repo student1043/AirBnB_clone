273ec24b484461601583d3d88e721abf7c301da7
student1043 <1043@holbertonschool.com>
2020-02-21 13:51:49 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,36 +7,36 @@ from models.base_model import BaseModel
 class FileStorage():
     """FileStorage"""
 
-    def __init__(self):
-        """initialization"""
-        self.__file_path = "file.json"
-        self.__objects = {}
+    __file_path = "file.json"
+    __objects = {}
 
     def all(self):
         """dict return """
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """new object"""
         object = obj.__class__.__name__ + "." + str(obj.id)
-        self.__objects[object] = obj
+        FileStorage.__objects[object] = obj
 
     def save(self):
         """serialize the objects to JSON"""
-        with open(self.__file_path, "w") as file:
+        with open(FileStorage.__file_path, "w") as file:
             dictionary = {}
-            for a, b in self.__objects.items():
+            for a, b in FileStorage.__objects.items():
                 dictionary[a] = b.to_dict()
-            ink = json.dumps(a)
+            ink = json.dumps(dictionary)
             file.write(ink)
 
     def reload(self):
         """File deserializing"""
         try:
-            file_reload = open(self.__file_path, "r")
-            file = json.loads(file_reload.read())
-            for k, v in file.items():
-                v = eval(v["__class__"])(**v)
-                self.__objects[k] = v
+            with open(FileStorage.__file_path, "r") as file:
+                info = json.load(file)
+                for k in info.values():
+                    mydic = k["__class__"]
+                    mydic = eval(mydic)
+                    object = mydic(**k)
+                    self.new(object)
         except:
             pass
